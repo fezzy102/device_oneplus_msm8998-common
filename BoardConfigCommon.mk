c6d0893e291a7f0835669cab1d05892c8f79d10f
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-14 20:43:11 +0100

    msm8998: enable interaction boost
    Change-Id: Id866b65aae97fce3073cd4e3556bcc708f6d5697

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -253,6 +253,7 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_USES_INTERACTION_BOOST := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab
